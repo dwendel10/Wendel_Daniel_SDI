e02cfa9159b03bcd451ec18f58641a883b1cbb86
dwendel10 <dwwendel@fullsail.edu>
2015-03-05 14:54:00 -0500

    Day 2 intro files
    Created html and js file for day number 2.

---
+++ Day2/js/script.js
@@ -0,0 +1,21 @@
+/*
+Daniel Wendel
+03/05/2015
+Section 00
+Day 2 intro
+ */
+
+//alert("Testing to see if connected");
+
+/*
+Example of a multiline comment
+anything inside of here will not be seen by the computer
+ */
+
+// Single line comment uses double slashes. ONLY this line will be ignored.
+
+// alert boxes- pop up box that will alert the user with information. The spelling of alert needs to be lower case.
+alert("Text that the user will see.");
+
+// files with . in front of them are hidden files
+// green means new in git. 
