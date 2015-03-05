dad3eaf6a6f475251670d6e787d62654490f46a6
dwendel10 <dwwendel@fullsail.edu>
2015-03-05 15:42:01 -0500

    Basic variables
    Learning about declaring and defining variables

--- Day2/js/script.js
+++ Day2/js/script.js
@@ -18,4 +18,30 @@ anything inside of here will not be seen by the computer
 alert("Text that the user will see.");
 
 // files with . in front of them are hidden files
-// green means new in git. 
+// green means new in git. red is change. white no change.
+
+//Console.log - shows information to the programmer
+// great for checking values and debugging
+//Also shows all errors
+console.log("This is the console");
+
+// declare a variable
+// use the key work of var <== needs to be lower case
+var whatever1;
+
+//define the variable
+//sets the value of the existing variable
+whatever1 = 42;
+
+//console.log the variable
+console.log("The variable whatever1 is");
+console.log(whatever1);
+
+//Declare and define a variable at the same time.
+var a = 2;
+console.log(a);
+
+a+3;
+a = a+3;
+console.log(a);
+
