ac141f9f8fdbcf4c71d9ca2b0bb2084156ccdaee
dwendel10 <dwwendel@fullsail.edu>
2015-03-05 18:54:39 -0500

    Slices of pie
    slices per person

--- Day2LabExpression/day2labjs.js
+++ Day2LabExpression/day2labjs.js
@@ -4,10 +4,58 @@
  Section 00
  Instructional Assignment: Expressions
  */
+
+
+alert("This is a test!")
+
+
+
+
 //calculate how much pizza each person attending the party will get.
+
 var slicesPerPizza = 8
 var peopleAtParty = 15
 var pizzasOrdered = 7
 
+// total amount of pizza - 56;
+
+
+//next line is finding out how much pizza there is in total.;
+
 var amountOfPizza = slicesPerPizza * pizzasOrdered;
 console.log(amountOfPizza);
+
+//3.73 pieces per person
+
+//dividing the amount of pizza by how many people are at the party.;
+
+var pizzaPerPerson = amountOfPizza / peopleAtParty;
+console.log("The amount of pizza per person is " + pizzaPerPerson);
+
+
+//Pizza for sparky
+
+
+
+
+
+
+
+
+
+
+
+//Average shopping bill. Calculate the average amount spent on groceries.;
+
+
+var groceryBills = 5
+var spentOnGroceries = 150.00
+var weeklySpendings = spentOnGroceries / groceryBills;
+console.log("The amount spent on a weekly basis is " + weeklySpendings)
+
+
+
+
+
+
+
