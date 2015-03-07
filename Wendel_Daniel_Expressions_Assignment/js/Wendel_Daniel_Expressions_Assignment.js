c372978999883e2ea02bc3388fd16b1318af500f
dwendel10 <dwwendel@fullsail.edu>
2015-03-07 18:06:26 -0500

    Created Folder
    Created Expressions assignment folder

---
+++ Wendel_Daniel_Expressions_Assignment/js/Wendel_Daniel_Expressions_Assignment.js
@@ -0,0 +1,9 @@
+/*
+Daniel Wendel
+3-7-2015
+Section 00
+Expressions Assignment
+ */
+alert("Testing 1..2..3!!")
+// Creating a calculator that will tell you how much you spend on video games per monthly and yearly.
+
