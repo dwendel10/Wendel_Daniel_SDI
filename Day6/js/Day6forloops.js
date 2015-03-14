99090b2fd2af07ba2a2b39495824f95b9dee9a15
dwendel10 <dwwendel@fullsail.edu>
2015-03-14 16:58:47 -0400

    Learned about for loops
    Learned about for loops

--- Day6/js/Day6forloops.js
+++ Day6/js/Day6forloops.js
@@ -4,5 +4,51 @@ Section 00
 3-14-2015
 Day 6 for loops
  */
-alert("Cmon son");
+//alert("C'mon son");
+
+
+// Basic for loop:  for(initialization; condition; increment of change){}
+//we use i A LOT in loops because it stands for iteration
+
+for(var i = 0; i<20; i++){
+    console.log("The value of i is "+i);
+
+}
+
+
+//Break in a loop
+
+for(var j = 0; j<5; j++){
+   //Conditional to test if we should break the loop
+    //If the value of j is 3 then console.log out something different and stop the loop
+
+    if(j === 3){
+        console.log("J is 3");
+        break;
+    }
+
+    console.log("The value of j is "+j);
+
+
+}
+
+
+// Specific cases for each value
+
+for(var k = 0; k<3; k++){
+    if(k === 0){
+        console.log("The value is zero.");
+
+    } else if (k===1){
+        console.log("The value is one.");
+    } else if (k===2){
+        console.log("The value is two.");
+    }
+
+    
+}
+
+
+
+
 
