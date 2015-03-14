7d74c04227c8b31a6f6204eed0a8784ad7da363b
dwendel10 <dwwendel@fullsail.edu>
2015-03-14 17:27:20 -0400

    Day 6 learning validations
    Day 6 learning validations

--- Day6/js/Day6Validation.js
+++ Day6/js/Day6Validation.js
@@ -9,12 +9,41 @@ Day 6 Validation
 
 
 
+// Ask the user for a number
+var num1 = prompt("Please enter a number:");
+//Validate that the user typed in a number OR didn't leave it blank
+
+//validate using a while loop
+while(isNaN(num1) || num1==="" ){
+    //re prompt the user
+
+    if(num1===""){
+        //the user left it blank
+        num1 = prompt("Please do not leave blank. \n Please type in a number.");
+
+    } else if (isNaN(num1)){
+        num1 = prompt("Please only type in numbers.")
+
+    }
+
+}
 
 
 
+// Ask the user a yes or no question
+
+var userInput = prompt("Please enter yes or no:");
+//convert to lower case
+userInput = userInput.toLowerCase();
+
+//Validate
+while(userInput != "yes" && userInput != "no" && userInput != "nope" ){
+
+    //Re-prompt
+    userInput = prompt("Please only enter yes or no!\nPlease type in yes or no.");
 
 
-
+}
 
 
 
