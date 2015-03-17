182eba2c5793baa0a29467c73a41c36d47d71629
dwendel10 <dwwendel@fullsail.edu>
2015-03-17 12:19:04 -0400

    Created a do while
    Created a do while loop

--- Loops_Worksheet/js/Do_Whileloops.js
+++ Loops_Worksheet/js/Do_Whileloops.js
@@ -7,3 +7,13 @@
 //alert("up and running");
 
 
+
+
+
+var num = prompt("Please enter a number.");
+
+do{
+    num = prompt("You entered a not a number. Please only enter numbers.")
+
+
+}while(isNaN(num) || num === "" );
