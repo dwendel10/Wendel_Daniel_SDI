81348ab08e71b3d5742f85d3b00201e97d07e3a5
dwendel10 <dwwendel@fullsail.edu>
2015-03-17 14:09:18 -0400

    For loop zombies
    zombies eat brains!!

--- Day7/js/Day7Zombies.js
+++ Day7/js/Day7Zombies.js
@@ -6,3 +6,45 @@
  */
 
 //alert("Zombies!!!!!");
+
+
+
+//Zombie attack!!!!!
+
+// We have accedently created a zombie at full sail
+//A zombie can bite 4 people a day and turn them to zombies the next day.
+// The CDC wants to know how many zombies there will be in 8 days.
+
+
+//Givens that we know
+
+//How many zombies do we have
+var numZombies = 1;
+
+//How many bites per zombie per day
+
+var numBites = 4;
+
+//How many days that the CDC wants.
+
+var days = 8;
+
+
+//Create for loop to calculate zombie number
+
+for(var i = 1; i<= days; i++){
+
+    // how many new zombies get made everyday
+    var newZombies = numZombies * numBites;
+
+    //Add the new zombies to our existing horde.
+
+    numZombies+=newZombies;
+    // Console.log results
+    console.log("There are "+numZombies+" number of zombies on day #"+i+"!");
+
+}
+
+//How long will it take to get a million zombies.
+
+while
